273166513514a6f30fc0af3b3cbffb88271b1d38
K.Kiyokawa <koichi20110068@gmail.com>
2019-04-08 17:40:29 +0900

    Add lang utils

---
+++ src/main/java/edu/koichi/packs/utilities/Lang.java
@@ -0,0 +1,15 @@
+package edu.koichi.packs.utilities;
+
+public class Lang {
+  public static String capitalize(String str) {
+    return str.substring(0, 1).toUpperCase() + str.substring(1);
+  }
+
+  public static boolean isCapital(String str) {
+    return Character.isUpperCase(str.charAt(0));
+  }
+
+  public static String uncapitalize(String str) {
+    return str.substring(0, 1).toLowerCase() + str.substring(1);
+  }
+}
