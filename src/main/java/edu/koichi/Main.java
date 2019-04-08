c682d623e350b917fdd3ef42dfa2012ca388ca97
K.Kiyokawa <koichi20110068@gmail.com>
2019-04-08 18:33:11 +0900

    Temporary modify Main class

--- src/main/java/edu/koichi/Main.java
+++ src/main/java/edu/koichi/Main.java
@@ -10,18 +10,13 @@ import edu.koichi.packs.entities.*;
 
 public class Main {
   public static void main(String args[]) {
-    System.out.println(args[0]);
     Repository repo = new Repository(args[0]);
     List<Commit> commits = repo.getCommits();
     commits.forEach(c -> {
-      System.out.println(c.message);
-      // if (c.isBugfixCommit()) {
-      // System.out.println("this is bugfix commit");
-      // System.out.println(c.message);
-      // // c.insertedLines.forEach(l -> System.out.println(l));
-      // } else {
-      // System.out.println(c.message);
-      // }
+      if (c.isBugfixCommit()) {
+        System.out.println(c.message);
+        // c.insertedLines.forEach(l -> System.out.println(l));
+      }
     });
   }
 }
