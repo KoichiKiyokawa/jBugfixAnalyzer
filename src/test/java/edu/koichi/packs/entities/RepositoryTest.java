5912caa310684fd7ce3b74851ceb02319aa1f7e6
K.Kiyokawa <koichi20110068@gmail.com>
2019-04-20 01:36:47 +0900

    Add checkout and get source test to Repository

--- src/test/java/edu/koichi/packs/entities/RepositoryTest.java
+++ src/test/java/edu/koichi/packs/entities/RepositoryTest.java
@@ -45,4 +45,12 @@ public class RepositoryTest extends TestCase {
     String[] files = repo.getSourceFilenames();
     assertEquals(files.length, 10);
   }
+
+  public void testCheckoutAndSourceFilesCOunt() {
+    Commit secondCommit = repo.commits.get(1);
+    repo.checkout(secondCommit);
+    String[] files = repo.getSourceFilenames();
+    assertEquals(files.length, 9);
+    RunCommand.run("git checkout master", testRepoDir);
+  }
 }
