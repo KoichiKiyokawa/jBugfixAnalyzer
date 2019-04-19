c72b0fe4f29a38d9151c473ad9d8bb10f88c1139
K.Kiyokawa <koichi20110068@gmail.com>
2019-04-19 22:55:59 +0900

    Add Repository test

---
+++ src/test/java/edu/koichi/packs/entities/RepositoryTest.java
@@ -0,0 +1,43 @@
+package edu.koichi.packs.entities;
+
+import java.io.File;
+
+import edu.koichi.packs.utilities.RunCommand;
+import junit.framework.TestCase;
+
+public class RepositoryTest extends TestCase {
+  private final String testRepoDir = "../jBugfixAnalyzer-test";
+  private Repository repo;
+  private int initialCommitLength;
+  private final String testRepoURL = "https://github.com/KoichiKiyokawa/jBugfixAnalyzer-test.git";
+
+  public RepositoryTest() {
+    super();
+    initTestRepo();
+    String res = RunCommand.run("git log --oneline", testRepoDir); // count line
+    this.repo = new Repository(testRepoDir);
+    this.initialCommitLength = res.split("\n").length;
+  }
+
+  private void initTestRepo() {
+    File testRepo = new File(testRepoDir);
+    if (testRepo.exists()) {
+      RunCommand.run("git reset --hard origin/master", testRepoDir);
+    } else {
+      RunCommand.run(String.format("git clone %s", testRepoURL), "../");
+    }
+  }
+
+  public void testCommitLength() {
+    assertEquals(repo.commits.size(), initialCommitLength);
+  }
+
+  public void testCheckout() {
+    Commit secondCommit = repo.commits.get(1);
+    repo.checkout(secondCommit);
+    assertEquals(secondCommit.message, "Add testY");
+    Repository repo2 = new Repository(testRepoDir);
+    assertEquals(repo2.commits.size(), initialCommitLength - 1);
+    RunCommand.run("git checkout master", testRepoDir);
+  }
+}
