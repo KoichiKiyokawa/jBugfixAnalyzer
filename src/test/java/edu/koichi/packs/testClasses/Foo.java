c9bb537721afa3ba6e72a0daa6819f57e0a0f0f4
K.Kiyokawa <koichi20110068@gmail.com>
2019-05-03 16:54:42 +0900

    Update testClass of Foo

--- src/test/java/edu/koichi/packs/testClasses/Foo.java
+++ src/test/java/edu/koichi/packs/testClasses/Foo.java
@@ -6,7 +6,15 @@ public class Foo {
   void three() {
     this.x = 3;
     int y = 2;
+    int yy = 4;
     System.out.println(x + y);
+    System.out.println(x + yy);
+  }
+
+  void loop(int times) {
+    for (int i = 0; i < times; i++) {
+      System.out.println(i + " times");
+    }
   }
 
   void hello() {
