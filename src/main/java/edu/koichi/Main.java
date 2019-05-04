d9f95cee425bac25df1b309537f063e757c1c300
K.Kiyokawa <koichi20110068@gmail.com>
2019-05-04 22:38:08 +0900

    Update Main class to show exec time

--- src/main/java/edu/koichi/Main.java
+++ src/main/java/edu/koichi/Main.java
@@ -5,9 +5,15 @@ import edu.koichi.packs.verifications.VerifyRedundancy;
 
 public class Main {
   public static void main(String[] args) {
+    long start = System.currentTimeMillis();
+
     System.out.println(args[0]);
     Repository repo = new Repository(args[0]);
     VerifyRedundancy vRedundancy = new VerifyRedundancy(repo);
     vRedundancy.verify();
+
+    long end = System.currentTimeMillis();
+    System.out.println("------------------");
+    System.out.println((end - start) + "ms");
   }
 }
