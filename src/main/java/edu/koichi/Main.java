50fcdc7919122c04b925cfddc6d5aa221513eb7b
K.Kiyokawa <koichi20110068@gmail.com>
2019-05-04 22:54:26 +0900

    Update Main class to branch verification type

--- src/main/java/edu/koichi/Main.java
+++ src/main/java/edu/koichi/Main.java
@@ -2,15 +2,26 @@ package edu.koichi;
 
 import edu.koichi.packs.entities.*;
 import edu.koichi.packs.verifications.VerifyRedundancy;
+import edu.koichi.packs.verifications.VerifyRedundancyWithLevenshtein;;
 
 public class Main {
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
 
     String repoPath = args[0];
-    System.out.println(repoPath);
+    String verificationType = args[1];
+    System.out.println("repoPath : " + repoPath);
+    System.out.println("verificationType : " + verificationType);
     Repository repo = new Repository(repoPath);
-    new VerifyRedundancy(repo).verify();
+
+    switch (verificationType) {
+    case "VerifyRedundancy":
+      new VerifyRedundancy(repo).verify();
+    case "VerifyRedundancyWithLevenshtein":
+      new VerifyRedundancyWithLevenshtein(repo).verify();
+    default:
+      new VerifyRedundancy(repo).verify();
+    }
 
     long end = System.currentTimeMillis();
     System.out.println("------------------");
