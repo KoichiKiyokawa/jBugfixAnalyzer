300ae41c95bbc262609a9650c2b5032ec2bc937f
K.Kiyokawa <koichi20110068@gmail.com>
2019-05-04 22:27:50 +0900

    Update SourceTest

--- src/test/java/edu/koichi/packs/entities/SourceTest.java
+++ src/test/java/edu/koichi/packs/entities/SourceTest.java
@@ -1,7 +1,17 @@
 package edu.koichi.packs.entities;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import spoon.processing.FileGenerator;
+import spoon.refactoring.Refactoring;
+import spoon.reflect.code.CtLocalVariable;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.visitor.filter.LineFilter;
 
 public class SourceTest {
   @Test
@@ -13,5 +23,31 @@ public class SourceTest {
     src.showStatements();
     System.out.println("---show code---");
     src.showCode();
+  }
+
+  @Ignore
+  public void testvar() {
+    Source src = new Source("src/test/java/edu/koichi/packs/testClasses/Foo.java");
+    FileGenerator fGenerator = src.launcher.getFactory().getEnvironment().getDefaultFileGenerator();
+  }
+
+  @Test
+  public void testGatheringAllVariables() {
+    Source src = new Source("src/test/java/edu/koichi/packs/testClasses/Foo.java");
+    // フィールドも含めた変数を抜き出すことができる
+    List<CtStatement> elements = src.model.getElements(new LineFilter());
+    // List<CtStatement> elements = src.model.getElements(new LineFilter());
+    // elements.forEach(System.out::println);
+
+    for (int i = 0; i < elements.size(); i++) {
+      // TODO: fieldをリネームすることができない
+      if (!(elements.get(i) instanceof CtLocalVariable)) {
+        continue;
+      }
+      Refactoring.changeLocalVariableName((CtLocalVariable) elements.get(i), "$" + i);
+    }
+    // elements.forEach(System.out::println);
+    src.launcher.getEnvironment().setAutoImports(true);
+    src.model.getElements(new LineFilter()).forEach(System.out::println);
   }
 }
